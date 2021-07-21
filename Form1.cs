bc44598d8f13016cde83697e672c51e02c4dd6a2
Hannah Kyme <63216601+hannahhkyme@users.noreply.github.com>
2021-07-21 15:59:20 -0700

    need to update weights for each PF

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,14 @@ namespace MultiplePF
         List<double> w2yList_pf1 = new List<double>();
         List<double> w3xList_pf1 = new List<double>();
         List<double> w3yList_pf1 = new List<double>();
+
+        List<double> w1xList_pf2 = new List<double>();
+        List<double> w1yList_pf2 = new List<double>();
+        List<double> w2xList_pf2 = new List<double>();
+        List<double> w2yList_pf2 = new List<double>();
+        List<double> w3xList_pf2 = new List<double>();
+        List<double> w3yList_pf2 = new List<double>();
+
         int NUMBER_OF_ROBOTS;
         int NUMBER_OF_SHARKS;
         int NUMBER_OF_PARTICLEFILTERS;
@@ -44,35 +52,64 @@ namespace MultiplePF
             
         }
 
-        private void update_weight_lists()
+        private void update_weight_lists(int index)
         {
-            w1xList_pf1 = particle_filter.w1_list_x;
-            w1yList_pf1 = particle_filter.w1_list_y;
-            w2xList_pf1 = particle_filter.w2_list_x;
-            w2yList_pf1 = particle_filter.w2_list_y;
-            w3yList_pf1 = particle_filter.w3_list_y;
-            w3xList_pf1 = particle_filter.w3_list_x;
+            if( index == 0)
+            {
+                w1xList_pf1 = ParticleFilterList[index].w1_list_x;
+                w1yList_pf1 = ParticleFilterList[index].w1_list_y;
+                w2xList_pf1 = ParticleFilterList[index].w2_list_x;
+                w2yList_pf1 = ParticleFilterList[index].w2_list_y;
+                w3yList_pf1 = ParticleFilterList[index].w3_list_y;
+                w3xList_pf1 = ParticleFilterList[index].w3_list_x;
+            }
+            if (index == 2)
+            { 
+                w1xList_pf2 = ParticleFilterList[index].w1_list_x;
+                w1yList_pf2 = ParticleFilterList[index].w1_list_y;
+                w2xList_pf2 = ParticleFilterList[index].w2_list_x;
+                w2yList_pf2 = ParticleFilterList[index].w2_list_y;
+                w3yList_pf2 = ParticleFilterList[index].w3_list_y;
+                w3xList_pf2 = ParticleFilterList[index].w3_list_x;
+            }
+           
         }
-        public void update_real_range_list()
+        public void update_real_range_list(int SharkNumber)
         {
             this.real_range_list = new List<double>();
             for (int i = 0; i < MyGlobals.robot_list.Count; i++)
             {
-                double real_range1 = MyGlobals.robot_list[i].calc_range_error_real(MyGlobals.shark_list[0]);
+                double real_range1 = MyGlobals.robot_list[i].calc_range_error_real(MyGlobals.shark_list[SharkNumber]);
                 this.real_range_list.Add(real_range1);
             }
         }
 
-        public void update_noisy_range_list()
+        public void update_noisy_range_list(int SharkNumber)
         {
             this.real_range_list = new List<double>();
             for (int i = 0; i < MyGlobals.robot_list.Count; i++)
             {
-                double real_range1 = MyGlobals.robot_list[i].calc_range_error(MyGlobals.shark_list[0]);
+                double real_range1 = MyGlobals.robot_list[i].calc_range_error(MyGlobals.shark_list[SharkNumber]);
                 this.real_range_list.Add(real_range1);
             }
         }
 
+        public int what_shark(int index)
+        {
+            int sharkNumber = 0;
+            if (index%NUMBER_OF_SHARKS == 0)
+            {
+                sharkNumber = NUMBER_OF_SHARKS;
+            }
+            else 
+            {
+                sharkNumber = index % NUMBER_OF_SHARKS;
+            }
+            int sharkIndex = sharkNumber - 1;
+            return sharkIndex;
+            // return sharkNUmber
+
+        }
         public void create_robots()
         {
             for (int i = 0; i < NUMBER_OF_ROBOTS; i++)
@@ -136,56 +173,66 @@ namespace MultiplePF
 
             create_simulation();
             createParticleFilters();
-
-            this.update_noisy_range_list();
-            particle_filter.update_weights(this.real_range_list);
-            particle_filter.NUMBER_OF_AUVS = MyGlobals.robot_list.Count;
+            //updates PFs
+            int index = 0;
+            foreach (ParticleFilter p1 in ParticleFilterList)
+            {
+                index += 1;
+                int sharkNumber = what_shark(index);
+                this.update_noisy_range_list(sharkNumber);
+                p1.update_weights(this.real_range_list);
+                p1.NUMBER_OF_AUVS = MyGlobals.robot_list.Count;
+            }
             double count = 0;
             while (stopHere)
             {
                 count += 10;
                 if(count %80 == 0)
-                {
-                    MyGlobals.shark_list[0].update_shark();
+                {   
+                    for (int i = 0; i < NUMBER_OF_SHARKS; ++i)
+                    {
+                        MyGlobals.shark_list[i].update_shark();
+                    }
                 }
                 //get real_measurements
-
+                // work on updating PF and real_range_measurements for each PF
                 get_measurements();
-
-                particle_filter.update();
-                particle_filter.update_weights(this.real_range_list);
-                particle_filter.correct();
-
-                List<double> predict_shark_location = new List<double>();
-                predict_shark_location = particle_filter.predicting_shark_location();
-
-
-                particle_filter.weight_list_x();
-                particle_filter.weight_list_y();
+                foreach (ParticleFilter p1 in ParticleFilterList)
+                {
+                    p1.update();
+                    p1.update_weights(this.real_range_list);
+                    p1.correct();  
+                }
+                
+                
+                foreach (ParticleFilter p1 in ParticleFilterList)
+                {
+                    p1.weight_list_x();
+                    p1.weight_list_y();
+                }
+                
 
 
                 // make coordinate list 
                 update_weight_lists();
 
+            }
                 // update_robot_location();
 
                 // range_error creator
                 // create_range_error();
-
-
-
-                if (chart1.IsHandleCreated)
-                {
-                    this.Invoke((MethodInvoker)delegate { UpdateMap(); });
-                }
-                else
-                {
-                    //......
-                }
-
-
-                Thread.Sleep(100);
             }
+            if (chart1.IsHandleCreated)
+            {
+                this.Invoke((MethodInvoker)delegate { UpdateMap(); });
+            }
+            else
+            {
+                //......
+            }
+
+
+            Thread.Sleep(100);
         }
          private void UpdateMap()
         {
@@ -193,12 +240,16 @@ namespace MultiplePF
             chart1.Series["Weight1"].Points.Clear();
             chart1.Series["Weight2"].Points.Clear();
             chart1.Series["Weight3"].Points.Clear();
+            chart1.Series["2ndWeight1"].Points.Clear();
+            chart1.Series["2ndWeight2"].Points.Clear();
+            chart1.Series["2ndWeight3"].Points.Clear();
             chart1.Series["Shark"].Points.Clear();
+            chart1.Series["Shark2"].Points.Clear();
             chart1.Series["AUV1"].Points.Clear();
             chart1.Series["AUV2"].Points.Clear();
             //chart1.Series["AUV3"].Points.Clear();
             chart1.Series["Shark"].Points.AddXY(MyGlobals.shark_list[0].X, MyGlobals.shark_list[0].Y);
-
+            chart1.Series["Shark2"].Points.AddXY(MyGlobals.shark_list[1].X, MyGlobals.shark_list[1].Y);
             //add in one of pf2
             for (int i = 0; i < w1xList_pf1.Count; ++i)
             {
@@ -211,6 +262,21 @@ namespace MultiplePF
             for (int i = 0; i < w3xList_pf1.Count; ++i)
             {
                 chart1.Series["Weight3"].Points.AddXY(w3xList_pf1[i], w3yList_pf1[i]);
+
+            }
+
+
+            for (int i = 0; i < w1xList_pf2.Count; ++i)
+            {
+                chart1.Series["2ndWeight1"].Points.AddXY(w1xList_pf2[i], w1yList_pf2[i]);
+            }
+            for (int i = 0; i < w2xList_pf2.Count; ++i)
+            {
+                chart1.Series["2ndWeight2"].Points.AddXY(w2xList_pf2[i], w2yList_pf2[i]);
+            }
+            for (int i = 0; i < w3xList_pf2.Count; ++i)
+            {
+                chart1.Series["2ndWeight3"].Points.AddXY(w3xList_pf2[i], w3yList_pf2[i]);
             }
 
            //double hey = particle_filter.r1.robot_list_x.Count;
